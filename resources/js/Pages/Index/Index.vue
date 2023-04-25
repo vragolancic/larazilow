36481fdac77b47d256120d3176c6162c84c3fb10
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2023-04-25 22:49:04 +0200

    Glavna instalacija

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Index</div>
+  <Link href="/hello">ShowPage</Link>
+  <div>Props: {{ message }}</div>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+
+defineProps({
+  message : String,
+})
+</script>
+
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Show ttt</div>
+  <Link href="/">ShowIndex</Link>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+</script>
+
